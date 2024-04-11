2680280f5284a20f0f2a4a12545fc2955347ea25
VikramJA99 <johnvickyvikram@gmail.com>
2024-04-11 15:25:39 +0530

    Update Db
    Signed-off-by: VikramJA99 <johnvickyvikram@gmail.com>

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise')
 
 const mysqlPool = mysql.createPool({
   host:'sql6.freesqldatabase.com',
-  user:'sql6683525',
-  password:'Ar72pcHStF',
-  database:'sql6683525',
+  user:'sql6698247',
+  password:'Lya4pa5Ets',
+  database:'sql6698247',
   
 })
 
